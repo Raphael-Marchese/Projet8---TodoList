b0b87eba14fceaafb6f3d9b458f5bd353fed0683
Raphael Marchese <103203222+Raphael-Marchese@users.noreply.github.com>
2025-08-28 13:50:50 +0200

    Feat/improve unit test coverage (#11)
    * Task controller tests
    
    * Add endlines
    
    * remove endlines
    
    * Remove test coverage
    
    * Add entity tests and modify compose.test.yaml and makefile to make command working good
    
    * Add endline
    
    * Improve user controller tests
    
    * Add new test coverage measure

---
+++ tests/units/AppBundle/Entity/TaskTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\units\AppBundle\Entity;
+
+use AppBundle\Entity\Task;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    public function testGetterSettersAndToggle() {
+        $task = new Task;
+        $task->setTitle('Test task');
+        $task->setContent('Test content');
+        $task->setCreatedAt(new DateTime("1988-10-17"));
+        $this->assertEquals('Test task', $task->getTitle());
+        $this->assertEquals('Test content', $task->getContent());
+        $this->assertEquals(new DateTime("1988-10-17"), $task->getCreatedAt());
+        $isDone = $task->isDone();
+        $task->toggle(!$isDone);
+        $this->assertNotEquals($isDone, $task->isDone());
+    }
+}
